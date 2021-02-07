4eeb65d77d1e977af0db89389f1f4eb4cfd99a25
Iltotore <rafbodaha@gmail.com>
2021-02-07 19:47:26 +0100

    test: Add ScalaTest for Dotty
    Signed-off-by: Iltotore <rafbodaha@gmail.com>

--- main/test/src/io/github/iltotore/scalalint/test/ConstraintSpec.scala
+++ main/test/src/io/github/iltotore/scalalint/test/ConstraintSpec.scala
@@ -1,19 +1,20 @@
 package io.github.iltotore.scalalint.test
 
+import io.github.iltotore.scalalint.constraint.numeric._
 import org.scalatest._
 import flatspec._
 import matchers._
 
 class ConstraintSpec extends AnyFlatSpec with should.Matchers {
 
-  "The Natural constraint" should "throw an error if the number isn't natural" in {
-    val inputs = Map(
-      -1 -> false,
-      1 -> true,
-      -1.5 -> false,
-      1.5 -> false
+  "The Natural constraint" should "return an error message if the number isn't natural" in {
+    val inputs: Map[Double, Boolean] = Map(
+      -1.0 -> true,
+      1.0 -> false,
+      -1.5 -> true,
+      1.5 -> true
     )
     
-    for(input <- inputs) assert(Natural.assert(input(0)) == input(1))
+    for(input <- inputs) Natural.assert(input(0)).isDefined shouldBe input(1)
   }
 }

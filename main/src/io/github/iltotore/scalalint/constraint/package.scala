90f6c24375eb0b4ae2c7cf5f48901b563824ad15
Iltotore <rafbodaha@gmail.com>
2021-02-08 19:59:21 +0100

    feat: Add possibility to mix constraints together
    Signed-off-by: Iltotore <rafbodaha@gmail.com>

--- main/src/io/github/iltotore/scalalint/constraint/package.scala
+++ main/src/io/github/iltotore/scalalint/constraint/package.scala
@@ -3,9 +3,14 @@ package io.github.iltotore.scalalint
 import scala.language.implicitConversions
 
 package object constraint {
-  
+
   implicit def valueToConstrained[T, C <: ConstraintAnchor](value: T)(using Constraint[T, C]): Constrained[T, C] =
     new Constrained[T, C](value)
-  
+
   implicit def constrainedToValue[T](constrained: Constrained[T, _ <: ConstraintAnchor]): T = constrained.value
+
+  def composedConstraint[T, C <: ConstraintAnchor, D <: ConstraintAnchor](using first: Constraint[T, C], second: Constraint[T, D]): Constraint[T, C & D] = new Constraint[T, C & D] {
+
+    override def assert(value: T): Option[String] = first.assert(value).orElse(second.assert(value))
+  }
 }

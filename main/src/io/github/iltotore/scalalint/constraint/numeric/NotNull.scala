1ea60139d35f8ece2536948223bde5c57761d764
Iltotore <rafbodaha@gmail.com>
2021-02-12 09:01:25 +0100

    feat: Add an inline version of NotNull as an example
    Signed-off-by: Iltotore <rafbodaha@gmail.com>

--- main/src/io/github/iltotore/scalalint/constraint/numeric/NotNull.scala
+++ main/src/io/github/iltotore/scalalint/constraint/numeric/NotNull.scala
@@ -6,5 +6,12 @@ trait NotNull extends ConstraintAnchor
 
 implicit object NotNull extends Constraint[Double, NotNull] {
 
-  override def assert(value: Double): Option[String] = if(value == 0) Some("$value shouldn't be zero") else None
+  override def assert(value: Double): Option[String] = if(value == 0) Some(s"$value shouldn't be zero") else None
+  
+  trait CompileTime extends ConstraintAnchor
+  
+  object CompileTime extends CompileTimeConstraint[Double, NotNull.CompileTime] {
+
+    override inline def assertCompileTime(inline value: Double): Option[String] = if(value == 0) Some(s"$value shouldn't be zero") else None
+  }
 }

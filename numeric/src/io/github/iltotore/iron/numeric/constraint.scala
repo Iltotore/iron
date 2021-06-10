39e237ded9b96cac97c812cc9d2c0e4dd7e3c6bb
Iltotore <rafbodaha@gmail.com>
2021-06-10 12:08:37 +0200

    fix: Remove unnecessary generic input type

--- numeric/src/io/github/iltotore/iron/numeric/constraint.scala
+++ numeric/src/io/github/iltotore/iron/numeric/constraint.scala
@@ -11,22 +11,22 @@ object constraint {
   type <[A, B] = A ==> Lesser[B]
   type Negative[A] = A ==> Lesser[0]
 
-  class LesserConstraint[A <: Double, V <: Double] extends Constraint[A, Lesser[V]] {
-    override inline def assert(value: A): Boolean = value < constValue[V]
+  class LesserConstraint[V <: Double] extends Constraint[Double, Lesser[V]] {
+    override inline def assert(value: Double): Boolean = value < constValue[V]
   }
 
-  inline given[A <: Double, V <: Double]: LesserConstraint[A, V] = new LesserConstraint
+  inline given[V <: Double]: LesserConstraint[V] = new LesserConstraint
 
 
   trait Greater[V]
   type >[A, B] = A ==> Greater[B]
   type Positive[A] = A ==> Greater[0]
 
-  class GreaterConstraint[A <: Double, V <: Double] extends Constraint[A, Greater[V]] {
-    override inline def assert(value: A): Boolean = value > constValue[V]
+  class GreaterConstraint[V <: Double] extends Constraint[Double, Greater[V]] {
+    override inline def assert(value: Double): Boolean = value > constValue[V]
   }
 
-  inline given[A <: Double, V <: Double]: GreaterConstraint[A, V] = new GreaterConstraint
+  inline given[V <: Double]: GreaterConstraint[V] = new GreaterConstraint
 
 
 }

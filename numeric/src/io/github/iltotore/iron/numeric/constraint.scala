4559954a25f5da060e25985345cc1673ce33bec1
Iltotore <rafbodaha@gmail.com>
2021-06-13 19:21:41 +0200

    feat: Use operator-like sugar for Positive and Negative

--- numeric/src/io/github/iltotore/iron/numeric/constraint.scala
+++ numeric/src/io/github/iltotore/iron/numeric/constraint.scala
@@ -9,7 +9,7 @@ object constraint {
 
   trait Lesser[V]
   type <[A, B] = A ==> Lesser[B]
-  type Negative[A] = A ==> Lesser[0]
+  type Negative[A] = A < 0
 
   class LesserConstraint[A <: Number, V <: Number] extends Constraint[Number, Lesser[V]] {
     override inline def assert(value: Double): Boolean = value < constValue[V]
@@ -20,7 +20,7 @@ object constraint {
 
   trait Greater[V]
   type >[A, B] = A ==> Greater[B]
-  type Positive[A] = A ==> Greater[0]
+  type Positive[A] = Int > 0
 
   class GreaterConstraint[A <: Number, V <: A] extends Constraint[A, Greater[V]] {
     override inline def assert(value: A): Boolean = NumberOrdering.gt(value, constValue[V])

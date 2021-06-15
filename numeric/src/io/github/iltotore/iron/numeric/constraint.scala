8a723b720f41ce0bd9bfe7da7ee6c6f49cc80b4b
Iltotore <rafbodaha@gmail.com>
2021-06-15 11:08:44 +0200

    fix: Remove Positive and Negative to avoid unexpected implicit conversion

--- numeric/src/io/github/iltotore/iron/numeric/constraint.scala
+++ numeric/src/io/github/iltotore/iron/numeric/constraint.scala
@@ -9,19 +9,17 @@ object constraint {
 
   trait Less[V]
   type <[A, B] = A ==> Less[B]
-  type Negative[A] = Less[0]
 
   class LessConstraint[A <: Number, V <: A] extends Constraint[A, Less[V]] {
-    override inline def assert(value: A): Boolean = NumberOrdering.lt(value)
+    override inline def assert(value: A): Boolean = NumberOrdering.lt(value, constValue[V])
   }
 
-  inline given[A <: Number, V <: A]: LessConstraint[V] = new LessConstraint
+  inline given[A <: Number, V <: A]: LessConstraint[A, V] = new LessConstraint
 
 
   trait Greater[V]
   type >[A, B] = A ==> Greater[B]
-  type Positive[A] = Greater[0]
-  type Natural = Int ==> Positive
+  type Natural = Int > 0
 
   class GreaterConstraint[A <: Number, V <: A] extends Constraint[A, Greater[V]] {
     override inline def assert(value: A): Boolean = NumberOrdering.gt(value, constValue[V])
@@ -31,12 +29,11 @@ object constraint {
 
   trait Divisible[V]
   type %[A, B] = A ==> Divisible[B]
-  type Even = Divisible[2]
+  type Even = Int ==> Divisible[2]
 
   class DivisibleConstraint[A <: Number, V <: A] extends Constraint[A, Divisible[V]] {
-    override inline def assert(value: A): Boolean = modulo(value, constValue[V])
+    override inline def assert(value: A): Boolean = modulo(value, constValue[V]) == 0
   }
 
-  inline given[A <: Number, V <: A]: DivisibleConstraint[V] = new DivisibleConstraint
-
+  inline given[A <: Number, V <: A]: DivisibleConstraint[A, V] = new DivisibleConstraint
 }

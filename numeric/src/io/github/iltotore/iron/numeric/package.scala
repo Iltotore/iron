5356787cb27073a0abf24316d9b60c98728aad11
Iltotore <rafbodaha@gmail.com>
2021-06-20 10:55:37 +0200

    refactor: Remove Zero type

--- numeric/src/io/github/iltotore/iron/numeric/package.scala
+++ numeric/src/io/github/iltotore/iron/numeric/package.scala
@@ -4,25 +4,6 @@ import io.github.iltotore.iron.ordering.InlinedOrdering
 
 package object numeric {
 
-  type Zero[T] = Zero.Zero[T]
-
-  inline given Zero[Byte] = Zero(0)
-
-  inline given Zero[Short] = Zero(0)
-
-  inline given Zero[Int] = Zero(0)
-
-  inline given Zero[Long] = Zero(0)
-
-  inline given Zero[Float] = Zero(0f)
-
-  inline given Zero[Double] = Zero(0d)
-
-  inline given Zero[BigInt] = Zero(BigInt(0))
-
-  inline given Zero[BigDecimal] = Zero(BigDecimal(0))
-
-
   type Number = Byte | Short | Int | Long | Float | Double
 
   transparent inline def modulo(x: Number, y: Number): Int = inline x match {

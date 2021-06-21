3b4d98e4ca979710e699e9a3402c471520183543
Iltotore <rafbodaha@gmail.com>
2021-06-21 15:56:20 +0200

    feat: Add runtime fallback behaviour

--- main/src/io/github/iltotore/iron/compileTime.scala
+++ main/src/io/github/iltotore/iron/compileTime.scala
@@ -5,28 +5,34 @@ import scala.quoted._
 
 object compileTime {
 
-  inline def preAssert(inline value: Boolean): Boolean = ${preAssertImpl('value)}
+  inline def preAssert(inline value: Boolean): Unit = ${preAssertImpl('value)}
 
-  private def preAssertImpl(expr: Expr[Boolean])(using quotes: Quotes): Expr[Boolean] = {
+  private def preAssertImpl(expr: Expr[Boolean])(using quotes: Quotes): Expr[Unit] = {
 
     expr.value match {
 
-      case Some(false) => quotes.reflect.report.error("Compile-time assertion failed", expr)
+      case Some(false) => quotes.reflect.report.error("Compile time assertion failed", expr)
+        '{()}
 
       case None => System.getProperty("iron.fallback", "error") match {
 
         case "error" => quotes.reflect.report.error("Unable to evaluate assertion at compile time", expr)
+          '{()}
 
         case "warn" => quotes.reflect.report.warning("Unable to evaluate assertion at compile time", expr)
+          fallback(expr)
 
-        case "allow" =>
+        case "allow" => fallback(expr)
 
         case unknown => quotes.reflect.report.error(s"Unknown option: $unknown. Use error|warn|allow")
+          '{()}
       }
 
-      case _ =>
+      case _ => '{()}
     }
+  }
 
-    expr
+  private def fallback(expr: Expr[Boolean])(using Quotes): Expr[Unit] = '{
+    if(!$expr) throw new IllegalArgumentException("Type assertion failed")
   }
 }

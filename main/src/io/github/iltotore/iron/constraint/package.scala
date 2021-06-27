a970d5d740d323c684f5dd672ebf0bb72ea5299d
Iltotore <rafbodaha@gmail.com>
2021-06-27 20:22:21 +0200

    feat: Add Refined[A] alias for Either[IllegalValueError[A], A]

--- main/src/io/github/iltotore/iron/constraint/package.scala
+++ main/src/io/github/iltotore/iron/constraint/package.scala
@@ -1,7 +1,6 @@
 package io.github.iltotore.iron
 
-import io.github.iltotore.iron._
-import scala.compiletime._
+import io.github.iltotore.iron.{Constrained, compileTime}
 import scala.language.implicitConversions
 import math.Ordering.Implicits.infixOrderingOps
 
@@ -9,23 +8,17 @@ package object constraint {
 
   /**
    * Implicit assertion check
-   * @param value the value passed to the assertion
+   *
+   * @param value      the value passed to the assertion
    * @param constraint the applied type constraint
    * @tparam A the input type
    * @tparam B the constraint's dummy
    * @return the value as Constrained (meaning "asserted value")
+   * @note Due to a type inference bug of Scala 3, [[constrainedToValue]] was moved to the package object.
    */
   implicit inline def valueToConstrained[A, B](value: A)(using inline constraint: Constraint[A, B]): Constrained[A, B] = {
-    compileTime.preAssert(constraint.assert(value))
-    Constrained(value)
+    Constrained(compileTime.preAssert(value, constraint))
   }
 
-  /**
-   * Implicit conversion from Constrained[A, B] to its shadowed type
-   * @param constrained the Constrained to be cast from
-   * @tparam A the input type
-   * @tparam B the constraint's dummy
-   * @return constrained as A
-   */
-  implicit inline def constrainedToValue[A, B](constrained: Constrained[A, B]): A = constrained
+  //Due to a Dotty type inference bug,
 }

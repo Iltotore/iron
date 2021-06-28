245eee5078e369ae11a1e0798442a78e99adb9aa
Iltotore <rafbodaha@gmail.com>
2021-06-28 17:06:15 +0200

    feat: Add Equal and StrictEqual constraints

--- main/src/io/github/iltotore/iron/constraint/package.scala
+++ main/src/io/github/iltotore/iron/constraint/package.scala
@@ -2,7 +2,8 @@ package io.github.iltotore.iron
 
 import io.github.iltotore.iron.{Constrained, compileTime}
 import scala.language.implicitConversions
-import math.Ordering.Implicits.infixOrderingOps
+import scala.compiletime.constValue
+import scala.math.Ordering.Implicits.infixOrderingOps
 
 package object constraint {
 
@@ -20,5 +21,32 @@ package object constraint {
     Constrained(compileTime.preAssert(value, constraint))
   }
 
-  //Due to a Dotty type inference bug,
+  /**
+   * Constraint: checks if the input value strictly equals to V.
+   * @tparam V
+   */
+  trait StrictEqual[V]
+  type ==[A, V] = A ==> StrictEqual[V]
+
+  class StrictEqualConstraint[A, V <: A] extends Constraint[A, StrictEqual[V]] {
+
+    override inline def assert(value: A): Boolean = value == constValue[V]
+  }
+
+  inline given[A, V <: A]: StrictEqualConstraint[A, V] = new StrictEqualConstraint
+
+
+  /**
+   * Constraint: checks if the input value equals (using Any#equals) to V.
+   * @tparam V
+   * @note This constraint is runtime-only
+   */
+  trait Equal[V]
+
+  class EqualConstraint[A, V <: A] extends Constraint[A, Equal[V]] {
+
+    override inline def assert(value: A): Boolean = value equals constValue[V]
+  }
+
+  inline given[A, V <: A]: EqualConstraint[A, V] = new EqualConstraint
 }
--- main/test/src/io/github/iltotore/iron/test/ConstraintSpec.scala
+++ main/test/src/io/github/iltotore/iron/test/ConstraintSpec.scala
@@ -2,7 +2,7 @@ package io.github.iltotore.iron.test
 
 import org.scalatest._
 import flatspec._
-import io.github.iltotore.iron.constraint.valueToConstrained
+import io.github.iltotore.iron._, constraint._
 import matchers._
 
 class ConstraintSpec extends UnitSpec {
@@ -10,5 +10,13 @@ class ConstraintSpec extends UnitSpec {
   "A compile-time constraint" should "compile if assertion is passed" in {
     "valueToConstrained[Boolean, Dummy](true)" should compile
     "valueToConstrained[Boolean, Dummy](false)" shouldNot compile
+  }
+
+  "An Equal[V] constraint" should "compile if the argument == V" in {
+
+    def dummy(x: Int == 0): Unit = {}
+
+    "dummy(0)" should compile
+    "dummy(1)" shouldNot compile
   }
 }

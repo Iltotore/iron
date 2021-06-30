bbb4187aba8464925779b21abf6bf4611ebf7e3d
Iltotore <rafbodaha@gmail.com>
2021-06-30 14:44:43 +0200

    test: Separate compile time and runtime

--- main/test/src/io/github/iltotore/iron/test/CompileTimeSpec.scala
+++ main/test/src/io/github/iltotore/iron/test/CompileTimeSpec.scala
@@ -2,17 +2,17 @@ package io.github.iltotore.iron.test
 
 import org.scalatest._
 import flatspec._
-import io.github.iltotore.iron._, constraint._
+import io.github.iltotore.iron._, constraint.{_, given}
 import matchers._
 
-class ConstraintSpec extends UnitSpec {
+class CompileTimeSpec extends UnitSpec {
 
   "A compile-time constraint" should "compile if assertion is passed" in {
     "valueToConstrained[Boolean, Dummy](true)" should compile
     "valueToConstrained[Boolean, Dummy](false)" shouldNot compile
   }
 
-  "An Equal[V] constraint" should "compile if the argument == V" in {
+  "A StrictEqual[V] constraint" should "compile if the argument == V" in {
 
     def dummy(x: Int == 0): Unit = {}
 

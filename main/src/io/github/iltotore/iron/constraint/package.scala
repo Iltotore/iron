05686988076e05431e6868825b1c91370ba85d43
Iltotore <rafbodaha@gmail.com>
2021-07-07 11:05:43 +0200

    refactor: Change old refine to refineValue

--- main/src/io/github/iltotore/iron/constraint/package.scala
+++ main/src/io/github/iltotore/iron/constraint/package.scala
@@ -17,7 +17,7 @@ package object constraint {
    * @return the value as Constrained (meaning "asserted value")
    * @note Due to a type inference bug of Scala 3, [[constrainedToValue]] was moved to the package object.
    */
-  implicit inline def refine[A, B, C <: Constraint[A, B]](value: A)(using inline constraint: C): Constrained[A, B] = {
+  implicit inline def refineValue[A, B, C <: Constraint[A, B]](value: A)(using inline constraint: C): Constrained[A, B] = {
     Constrained(compileTime.preAssert(value, constraint))
   }
 

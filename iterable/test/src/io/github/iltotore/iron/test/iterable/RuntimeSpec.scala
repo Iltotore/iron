22e21eafc3a65bb55250b7b3644afd0e796cb076
Iltotore <rafbodaha@gmail.com>
2021-07-11 11:00:21 +0200

    feat: Add MinSize[V] and MaxSize[V] constraints

--- iterable/test/src/io/github/iltotore/iron/test/iterable/RuntimeSpec.scala
+++ iterable/test/src/io/github/iltotore/iron/test/iterable/RuntimeSpec.scala
@@ -5,12 +5,21 @@ import io.github.iltotore.iron.test.UnitSpec
 
 class RuntimeSpec extends UnitSpec {
 
-  "A MinSize[V] constraint" should "return Right if the argument has a size greater than V" in {
+  "A MinSize[V] constraint" should "return Right if the argument has a size greater or equal to V" in {
 
     def dummy(x: Iterable[Int] ==> MinSize[3]): Iterable[Int] ==> MinSize[3] = x
 
     assert(dummy(0 until 3).isRight)
     assert(dummy(0 until 5).isRight)
     assert(dummy(0 until 2).isLeft)
+  }
+
+  "A MaxSize[V] constraint" should "return Right if the argument has a size lesser or equal to V" in {
+
+    def dummy(x: Iterable[Int] ==> MaxSize[3]): Iterable[Int] ==> MaxSize[3] = x
+
+    assert(dummy(0 until 3).isRight)
+    assert(dummy(0 until 2).isRight)
+    assert(dummy(0 until 5).isLeft)
   }
 }

9c625545bcf5b6400acfdd281018f03865df2dfc
Iltotore <rafbodaha@gmail.com>
2021-07-15 08:45:45 +0200

    feat: Use cats.data.Validated

--- cats/src/io/github/iltotore/iron/catsSupport/package.scala
+++ cats/src/io/github/iltotore/iron/catsSupport/package.scala
@@ -1,15 +1,31 @@
 package io.github.iltotore.iron
 
-import cats.data.NonEmptyList
 import cats.Semigroup
+import cats.data.{ValidatedNec, ValidatedNel}
+import cats.syntax.either.*
 import io.github.iltotore.iron.constraint.IllegalValueError
 
+/**
+ * The package is named `catsSupport` to avoid clash with cats' root package.
+ */
 package object catsSupport {
 
-  type AccumulatedRefined[A] = Either[NonEmptyList[IllegalValueError[?]], A]
+  type RefinedNec[A] = ValidatedNec[IllegalValueError[?], A]
+  type RefinedNel[A] = ValidatedNel[IllegalValueError[?], A]
 
   extension [A](refined: Refined[A]) {
 
-    def accumulated: AccumulatedRefined[A] = refined.left.map(NonEmptyList.one)
+    /**
+     * Transforms this Refined[A] to a ValidatedNec.
+     * @return this value as RefinedNec[A]
+     */
+    def validatedNec: RefinedNec[A] = refined.toValidatedNec
+
+    /**
+     * Transforms this Refined[A] to a ValidatedNel.
+     * @return this value as RefinedNel[A]
+     */
+    def validatedNel: RefinedNel[A] = refined.toValidatedNel
+
   }
 }

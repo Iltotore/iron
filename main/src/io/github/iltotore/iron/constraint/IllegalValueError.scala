51d9d56c8be731703494203737c2f7592e42d388
Iltotore <rafbodaha@gmail.com>
2021-07-24 14:19:19 +0200

    feat: Add field-based IllegalValueError

--- main/src/io/github/iltotore/iron/constraint/IllegalValueError.scala
+++ main/src/io/github/iltotore/iron/constraint/IllegalValueError.scala
@@ -3,7 +3,20 @@ package io.github.iltotore.iron.constraint
 /**
  * Represents a type-level assertion failure.
  * @param input the invalid value
- * @param constraint the type constraint applied to [[input]]
+ * @param message the error message
  * @tparam A the input type
  */
-case class IllegalValueError[A](input: A, message: String) extends Error(message)
+case class IllegalValueError[A](input: A, message: String) extends Error(message) {
+
+  def toField(name: String): IllegalValueError.Field = IllegalValueError.Field(name, message)
+}
+
+object IllegalValueError {
+
+  /**
+   * A field-based variant of [[IllegalValueError]]. Useful for API requests/forms.
+   * @param name the field name
+   * @param message the error message
+   */
+  case class Field(name: String, message: String) extends Error(message)
+}

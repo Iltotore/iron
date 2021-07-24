f2e376e40752b4e0969cace44f3b86e2eab4afee
Iltotore <rafbodaha@gmail.com>
2021-07-24 14:44:27 +0200

    feat: Add Encoders for IllegalValueErrors

--- circe/src/io/github/iltotore/iron/circe/package.scala
+++ circe/src/io/github/iltotore/iron/circe/package.scala
@@ -1,10 +1,16 @@
 package io.github.iltotore.iron
 
-import io.circe.Decoder
-import io.github.iltotore.iron.constraint.{Constraint, refineValue}
+import io.circe.{Decoder, Encoder}
+import io.circe.generic.semiauto.deriveEncoder
+import io.github.iltotore.iron.constraint.{Constraint, IllegalValueError, refineValue}
 
 package object circe {
 
   inline given[A, B, C <: Constraint[A, B]](using inputDecoder: Decoder[A], c: C): Decoder[A ==> B] = inputDecoder
     .map(refineValue[A, B, C](_))
+
+  inline given [A](using Encoder[A]): Encoder[IllegalValueError[A]] = deriveEncoder
+
+  inline given Encoder[IllegalValueError.Field] = deriveEncoder
+
 }

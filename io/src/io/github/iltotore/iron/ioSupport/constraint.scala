01fc22b9175aa5cbafc10c570a48f7123e70a86a
Iltotore <rafbodaha@gmail.com>
2021-08-19 14:12:37 +0200

    feat: Add IsFile & IsDirectory constraints

--- io/src/io/github/iltotore/iron/ioSupport/constraint.scala
+++ io/src/io/github/iltotore/iron/ioSupport/constraint.scala
@@ -33,4 +33,56 @@ object constraint {
 
     override inline def getMessage(value: String): String = s"No such file or directoy: $value"
   }
+
+  /**
+   * Constraint: checks if the input is a regular file.
+   */
+  trait IsFile
+
+  given Constraint.RuntimeOnly[File, IsFile] with {
+
+    override inline def assert(value: File): Boolean = value.isFile
+
+    override inline def getMessage(value: File): String = s"Not a file: ${value.getPath}"
+  }
+
+  given Constraint.RuntimeOnly[Path, IsFile] with {
+
+    override inline def assert(value: Path): Boolean = Files.isRegularFile(value)
+
+    override inline def getMessage(value: Path): String = s"Not a file: $value"
+  }
+
+  given Constraint.RuntimeOnly[String, IsFile] with {
+
+    override inline def assert(value: String): Boolean = Files.isRegularFile(Paths.get(value))
+
+    override inline def getMessage(value: String): String = s"Not a file: $value"
+  }
+
+  /**
+   * Constraint: checks if the input is a directory.
+   */
+  trait IsDirectory
+
+  given Constraint.RuntimeOnly[File, IsDirectory] with {
+
+    override inline def assert(value: File): Boolean = value.isDirectory()
+
+    override inline def getMessage(value: File): String = s"Not a directory: ${value.getPath}"
+  }
+
+  given Constraint.RuntimeOnly[Path, IsDirectory] with {
+
+    override inline def assert(value: Path): Boolean = Files.isDirectory(value)
+
+    override inline def getMessage(value: Path): String = s"Not a directory: $value"
+  }
+
+  given Constraint.RuntimeOnly[String, IsDirectory] with {
+
+    override inline def assert(value: String): Boolean = Files.isDirectory(Paths.get(value))
+
+    override inline def getMessage(value: String): String = s"Not a directory: $value"
+  }
 }

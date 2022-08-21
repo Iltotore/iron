cb7374e5106a034c9989e300ecc7266ca4aaab74
Iltotore <rafbodaha@gmail.com>
2022-08-21 10:56:05 +0200

    feat: Add GreaterEqual alias

--- main/src/io/github/iltotore/iron/constraint/numeric.scala
+++ main/src/io/github/iltotore/iron/constraint/numeric.scala
@@ -2,12 +2,15 @@ package io.github.iltotore.iron.constraint
 
 import io.github.iltotore.iron.{==>, Constraint, Implication, Number}
 import io.github.iltotore.iron.ops.*
+import io.github.iltotore.iron.constraint.any.{*, given}
 import io.github.iltotore.iron.ordering.NumberOrdering
 
-import scala.compiletime.{constValue}
+import scala.compiletime.constValue
 import scala.compiletime.ops.any.ToString
 
 object numeric:
+
+  //TODO GreaterEqual, Less, LessEqual, Multiple
 
   final class Greater[V]
 
@@ -15,6 +18,10 @@ object numeric:
 
     override inline def test(value: A): Boolean = NumberOrdering.gt(value, constValue[V])
 
-    override inline def message: String = "Should be greater than " + compiletime.constValue[ToString[V]]
+    override inline def message: String = "Should be greater than " + stringValue[V]
 
   inline given[V1, V2](using V1 > V2 =:= true): (Greater[V1] ==> Greater[V2]) = Implication()
+  inline given[V1, V2](using V1 > V2 =:= true): (StrictEqual[V1] ==> Greater[V2]) = Implication()
+
+
+  type GreaterEqual[V] = (Greater[V] || StrictEqual[V]) DescribedAs ("Should be greater than or equal to " + V)

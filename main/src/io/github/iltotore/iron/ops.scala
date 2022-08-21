28135d35fbd6a007d40b2655f736002edc608148
Iltotore <rafbodaha@gmail.com>
2022-08-21 10:55:34 +0200

    feat: Add stringValue from type and wrapper for + (addition or concatenation)

--- main/src/io/github/iltotore/iron/ops.scala
+++ main/src/io/github/iltotore/iron/ops.scala
@@ -1,6 +1,7 @@
 package io.github.iltotore.iron
 
-import scala.compiletime.ops.*
+import scala.compiletime.constValue
+import scala.compiletime.ops.*, any.ToString
 
 object ops {
   
@@ -29,4 +30,14 @@ object ops {
     case (Int, Double) => double.<=[int.ToDouble[A], B]
     case (Double, Int) => double.<=[A, int.ToDouble[B]]
     case (Double, Double) => double.<=[A, B]
+
+  type +[A, B] = (A, B) match
+    case (Int, Int) => int.+[A, B]
+    case (Int, Double) => double.+[int.ToDouble[A], B]
+    case (Double, Int) => double.+[A, int.ToDouble[B]]
+    case (Double, Double) => double.+[A, B]
+    case (String, ?) => string.+[A, ToString[B]]
+    case (?, String) => string.+[ToString[A], B]
+  
+  inline def stringValue[A]: String = constValue[ToString[A]]
 }

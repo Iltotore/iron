019d0dd51cfba8a10d7e846aaec72c4775787204
Iltotore <rafbodaha@gmail.com>
2022-08-21 10:11:57 +0200

    feat: Add >=, <, <= operator aliases

--- main/src/io/github/iltotore/iron/ops.scala
+++ main/src/io/github/iltotore/iron/ops.scala
@@ -3,10 +3,30 @@ package io.github.iltotore.iron
 import scala.compiletime.ops.*
 
 object ops {
+  
+  //TODO >=, <, <=
 
   type >[A, B] = (A, B) match
     case (Int, Int) => int.>[A, B]
     case (Int, Double) => double.>[int.ToDouble[A], B]
     case (Double, Int) => double.>[A, int.ToDouble[B]]
     case (Double, Double) => double.>[A, B]
+
+  type >=[A, B] = (A, B) match
+    case (Int, Int) => int.>=[A, B]
+    case (Int, Double) => double.>=[int.ToDouble[A], B]
+    case (Double, Int) => double.>=[A, int.ToDouble[B]]
+    case (Double, Double) => double.>=[A, B]
+
+  type <[A, B] = (A, B) match
+    case (Int, Int) => int.<[A, B]
+    case (Int, Double) => double.<[int.ToDouble[A], B]
+    case (Double, Int) => double.<[A, int.ToDouble[B]]
+    case (Double, Double) => double.<[A, B]
+
+  type <=[A, B] = (A, B) match
+    case (Int, Int) => int.<=[A, B]
+    case (Int, Double) => double.<=[int.ToDouble[A], B]
+    case (Double, Int) => double.<=[A, int.ToDouble[B]]
+    case (Double, Double) => double.<=[A, B]
 }

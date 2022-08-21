3a12e6560d2e4ccb72ce72d3e82513d071daaadf
Iltotore <rafbodaha@gmail.com>
2022-08-21 11:04:13 +0200

    feat: Add Less and LessEqual constraints

--- main/src/io/github/iltotore/iron/constraint/numeric.scala
+++ main/src/io/github/iltotore/iron/constraint/numeric.scala
@@ -25,3 +25,19 @@ object numeric:
 
 
   type GreaterEqual[V] = (Greater[V] || StrictEqual[V]) DescribedAs ("Should be greater than or equal to " + V)
+
+
+  final class Less[V]
+
+  inline given[A <: Number, V <: A]: Constraint[A, Less[V]] with
+
+    override inline def test(value: A): Boolean = NumberOrdering.lt(value, constValue[V])
+
+    override inline def message: String = "Should be less than " + stringValue[V]
+
+  inline given[V1, V2](using V1 < V2 =:= true): (Less[V1] ==> Less[V2]) = Implication()
+
+  inline given[V1, V2](using V1 < V2 =:= true): (StrictEqual[V1] ==> Less[V2]) = Implication()
+
+
+  type LessEqual[V] = (Less[V] || StrictEqual[V]) DescribedAs ("Should be less than or equal to " + V)

341ad2d251d42edb2021e8f0e269ce24cf2aa4b8
Iltotore <rafbodaha@gmail.com>
2022-08-21 10:56:49 +0200

    fix: Already completed TODO in ops.scala

--- main/src/io/github/iltotore/iron/ops.scala
+++ main/src/io/github/iltotore/iron/ops.scala
@@ -4,8 +4,6 @@ import scala.compiletime.constValue
 import scala.compiletime.ops.*, any.ToString
 
 object ops {
-  
-  //TODO >=, <, <=
 
   type >[A, B] = (A, B) match
     case (Int, Int) => int.>[A, B]
@@ -38,6 +36,6 @@ object ops {
     case (Double, Double) => double.+[A, B]
     case (String, ?) => string.+[A, ToString[B]]
     case (?, String) => string.+[ToString[A], B]
-  
+
   inline def stringValue[A]: String = constValue[ToString[A]]
 }

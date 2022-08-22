d056b4ea871c62885f9425cbe88f5afb0eadf3a1
Iltotore <rafbodaha@gmail.com>
2022-08-22 20:02:33 +0200

    feat: Add arithmetic operator aliases
    Closes: #54

--- main/src/io/github/iltotore/iron/ops.scala
+++ main/src/io/github/iltotore/iron/ops.scala
@@ -11,42 +11,53 @@ object ops {
     case Float => 0f
     case Double => 0d
 
+  type Compatible[A, B] = A match
+    case Int =>
+      B match
+        case Int => (A, B)
+        case Long => (int.ToLong[A], B)
+        case Float => (int.ToFloat[A], B)
+        case Double => (int.ToDouble[A], B)
+    case Long =>
+      B match
+        case Int => (A, int.ToLong[B])
+        case Long => (A, B)
+        case Float => (long.ToFloat[A], B)
+        case Double => (long.ToDouble[A], B)
+    case Float =>
+      B match
+        case Int => (A, int.ToFloat[A])
+        case Long => (A, long.ToFloat[A])
+        case Float => (A, B)
+        case Double => (float.ToDouble[A], B)
+    case Double =>
+      B match
+        case Int => (A, int.ToDouble[A])
+        case Long => (A, long.ToDouble[A])
+        case Float => (A, float.ToDouble[A])
+        case Double => (A, B)
 
-  type >[A, B] = (A, B) match
-    case (Int, Int) => int.>[A, B]
-    case (Int, Double) => double.>[int.ToDouble[A], B]
-    case (Double, Int) => double.>[A, int.ToDouble[B]]
-    case (Double, Double) => double.>[A, B]
+  type NumOp[A, B, IntOp[_ <: Int, _ <: Int], LongOp[_ <: Long, _ <: Long], FloatOp[_ <: Float, _ <: Float], DoubleOp[_ <: Double, _ <: Double]] =
+    Compatible[A, B] match
+      case (Int, Int) => IntOp[A, B]
+      case (Long, Long) => LongOp[A, B]
+      case (Float, Float) => FloatOp[A, B]
+      case (Double, Double) => DoubleOp[A, B]
 
-  type >=[A, B] = (A, B) match
-    case (Int, Int) => int.>=[A, B]
-    case (Int, Double) => double.>=[int.ToDouble[A], B]
-    case (Double, Int) => double.>=[A, int.ToDouble[B]]
-    case (Double, Double) => double.>=[A, B]
-
-  type <[A, B] = (A, B) match
-    case (Int, Int) => int.<[A, B]
-    case (Int, Double) => double.<[int.ToDouble[A], B]
-    case (Double, Int) => double.<[A, int.ToDouble[B]]
-    case (Double, Double) => double.<[A, B]
-
-  type <=[A, B] = (A, B) match
-    case (Int, Int) => int.<=[A, B]
-    case (Int, Double) => double.<=[int.ToDouble[A], B]
-    case (Double, Int) => double.<=[A, int.ToDouble[B]]
-    case (Double, Double) => double.<=[A, B]
-
-  type %[A, B] = (A, B) match
-    case (Int, Int) => int.%[A, B]
-    case (Long, Long) => long.%[A, B]
+  type >[A, B] = NumOp[A, B, int.>, long.>, float.>, double.>]
+  type >=[A, B] = NumOp[A, B, int.>=, long.>=, float.>=, double.>=]
+  type <[A, B] = NumOp[A, B, int.<, long.<, float.<, double.<]
+  type <=[A, B] = NumOp[A, B, int.<=, long.<=, float.<=, double.<=]
 
   type +[A, B] = (A, B) match
-    case (Int, Int) => int.+[A, B]
-    case (Int, Double) => double.+[int.ToDouble[A], B]
-    case (Double, Int) => double.+[A, int.ToDouble[B]]
-    case (Double, Double) => double.+[A, B]
     case (String, ?) => string.+[A, ToString[B]]
     case (?, String) => string.+[ToString[A], B]
+    case _ => NumOp[A, B, int.+, long.+, float.+, double.+]
+
+  type -[A, B] = NumOp[A, B, int.-, long.-, float.-, double.-]
+  type *[A, B] = NumOp[A, B, int.*, long.*, float.*, double.*]
+  type /[A, B] = NumOp[A, B, int./, long./, float./, double./]
+  type %[A, B] = NumOp[A, B, int.%, long.%, float.%, double.%]
 
   inline def stringValue[A]: String = constValue[ToString[A]]
 

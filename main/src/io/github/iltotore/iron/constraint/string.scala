2aca213ad4e467dfbf0eb6dbb117c1218ceeedef
Iltotore <rafbodaha@gmail.com>
2022-09-15 17:13:38 +0200

    refactor: Move `string` constraints' macros in the same file

--- main/src/io/github/iltotore/iron/constraint/string.scala
+++ main/src/io/github/iltotore/iron/constraint/string.scala
@@ -2,8 +2,9 @@ package io.github.iltotore.iron.constraint
 
 import io.github.iltotore.iron.Constraint
 import io.github.iltotore.iron.constraint.any.{*, given}
-import io.github.iltotore.iron.ops.*
+import io.github.iltotore.iron.compileTime.*
 import scala.compiletime.constValue
+import scala.quoted.*
 
 /**
  * [[String]]-related constraints.
@@ -16,21 +17,36 @@ object string:
 
   inline given [V <: Int]: Constraint[String, MinLength[V]] with
 
-    override inline def test(value: String): Boolean = macros.checkMinLength(value, constValue[V])
+    override inline def test(value: String): Boolean = ${ checkMinLength('value, '{constValue[V]}) }
 
     override inline def message: String = "Should have a min length of " + stringValue[V]
 
+  private def checkMinLength(expr: Expr[String], lengthExpr: Expr[Int])(using Quotes): Expr[Boolean] =
+    (expr.value, lengthExpr.value) match
+      case (Some(value), Some(minLength)) => Expr(value.length >= minLength)
+      case _ => '{${expr}.length >= $lengthExpr}
+
   inline given [V <: Int]: Constraint[String, MaxLength[V]] with
 
-    override inline def test(value: String): Boolean = macros.checkMaxLength(value, constValue[V])
+    override inline def test(value: String): Boolean = ${checkMaxLength('value, '{constValue[V]})}
 
     override inline def message: String = "Should have a max length of " + stringValue[V]
 
+  private def checkMaxLength(expr: Expr[String], lengthExpr: Expr[Int])(using Quotes): Expr[Boolean] =
+    (expr.value, lengthExpr.value) match
+      case (Some(value), Some(maxLength)) => Expr(value.length <= maxLength)
+      case _ => '{${expr}.length <= $lengthExpr}
+
   inline given [V <: String]: Constraint[String, Contain[V]] with
 
-    override inline def test(value: String): Boolean = macros.checkContain(value, constValue[V])
+    override inline def test(value: String): Boolean = ${checkContain('value, '{constValue[V]})}
 
     override inline def message: String = "Should contain the string " + constValue[V]
+
+  private def checkContain(expr: Expr[String], partExpr: Expr[String])(using Quotes): Expr[Boolean] =
+    (expr.value, partExpr.value) match
+      case (Some(value), Some(part)) => Expr(value.contains(part))
+      case _ => '{${expr}.contains($partExpr)}
 
   /**
    * Tests if the given input is lower-cased.
@@ -39,9 +55,14 @@ object string:
 
   inline given Constraint[String, LowerCase] with
 
-    override inline def test(value: String): Boolean = macros.checkLowerCase(value)
+    override inline def test(value: String): Boolean = ${checkLowerCase('value)}
 
     override inline def message: String = "Should be lower cased"
+
+  def checkLowerCase(valueExpr: Expr[String])(using Quotes): Expr[Boolean] =
+    valueExpr.value match
+      case Some(value) => Expr(value equals value.toLowerCase)
+      case None => '{$valueExpr equals $valueExpr.toLowerCase}
 
   /**
    * Tests if the input is upper-cased.
@@ -50,9 +71,14 @@ object string:
 
   inline given Constraint[String, UpperCase] with
 
-    override inline def test(value: String): Boolean = macros.checkUpperCase(value)
+    override inline def test(value: String): Boolean = ${checkUpperCase('value)}
 
     override inline def message: String = "Should be upper cased"
+
+  private def checkUpperCase(valueExpr: Expr[String])(using Quotes): Expr[Boolean] =
+    valueExpr.value match
+      case Some(value) => Expr(value equals value.toUpperCase)
+      case None => '{$valueExpr equals $valueExpr.toUpperCase}
 
   /**
    * Tests if the input matches the given regex.
@@ -82,6 +108,11 @@ object string:
 
   inline given [V <: String]: Constraint[String, Match[V]] with
 
-    override inline def test(value: String): Boolean = macros.checkMatch(value, constValue[V])
+    override inline def test(value: String): Boolean = ${checkMatch('value, '{constValue[V]})}
 
     override inline def message: String = "Should match " + constValue[V]
+
+  def checkMatch(valueExpr: Expr[String], regexExpr: Expr[String])(using Quotes): Expr[Boolean] =
+    (valueExpr.value, regexExpr.value) match
+      case (Some(value), Some(regex)) => Expr(value.matches(regex))
+      case _ => '{$valueExpr.matches($regexExpr)}

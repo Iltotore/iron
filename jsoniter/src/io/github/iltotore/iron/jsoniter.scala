634b3c8019bdbd324497d3d268005af9ca146b79
kyri-petrou <kpe@dixa.com>
2022-12-15 08:55:59 +1100

    Use `constraint.test` when decoding to avoid boxing of primitives

--- jsoniter/src/io/github/iltotore/iron/jsoniter.scala
+++ jsoniter/src/io/github/iltotore/iron/jsoniter.scala
@@ -12,6 +12,9 @@ object jsoniter:
    */
   inline given makeCodec[A, B](using inline constraint: Constraint[A, B]): JsonValueCodec[A :| B] = new:
     private val codec = JsonCodecMaker.make[A]
-    def decodeValue(in: JsonReader, default: A :| B): A :| B = codec.decodeValue(in, default).refineEither[B].fold(in.decodeError, identity)
+    def decodeValue(in: JsonReader, default: A :| B): A :| B =
+      val decoded = codec.decodeValue(in, default)
+      if constraint.test(decoded) then decoded.asInstanceOf[A :| B]
+      else in.decodeError(constraint.message)
     def encodeValue(x: A :| B, out: JsonWriter): Unit = codec.encodeValue(x, out)
     def nullValue: A :| B = null.asInstanceOf[A :| B]

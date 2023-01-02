7c9895de4b8f910fb014d3744cf1526637eda8dd
Iltotore <rafbodaha@gmail.com>
2023-01-02 10:39:11 +0100

    feat: Add ZIO `refineValidation` extension

---
+++ zio/src/io/github/iltotore/iron/zio.scala
@@ -0,0 +1,17 @@
+package io.github.iltotore.iron
+
+import _root_.zio.NonEmptyChunk
+import _root_.zio.prelude.Validation
+
+object zio:
+
+  extension [A](value: A)
+
+    /**
+     * Refine the given value applicatively at runtime, resulting in a [[Validation]].
+     *
+     * @param constraint the constraint to test with the value to refine.
+     * @return a [[Valid]] containing this value as [[IronType]] or an [[Validation.Failure]] containing a [[NonEmptyChunk]] of error messages.
+     */
+    inline def refineValidation[C](using inline constraint: Constraint[A, C]): Validation[String, A] =
+      Validation.fromPredicateWith(constraint.message)(value.asInstanceOf[A :| C])(constraint.test(_))

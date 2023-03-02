8c1ff2afd46d81418fddb9b45f4a5dbb675321b5
Iltotore <rafbodaha@gmail.com>
2023-03-02 12:10:32 +0100

    feat: Add `StrictEqual` arbitrary

--- scalacheck/src/io/github/iltotore/iron/scalacheck/any.scala
+++ scalacheck/src/io/github/iltotore/iron/scalacheck/any.scala
@@ -1,14 +1,18 @@
 package io.github.iltotore.iron.scalacheck
 
 import io.github.iltotore.iron.*
-
-import org.scalacheck.Arbitrary
+import io.github.iltotore.iron.constraint.any.StrictEqual
+import org.scalacheck.*
 import org.scalacheck.Arbitrary.arbitrary
+
+import scala.compiletime.constValue
 
 object any extends LowPriorityArbitrary:
 
   inline given [A, C1, C2](using C1 ==> C2, C2 ==> C1, Arbitrary[A :| C1]): Arbitrary[A :| C2] =
     summon[Arbitrary[A :| C1]].asInstanceOf
+
+  inline given [A, V <: A]: Arbitrary[A :| StrictEqual[V]] = Arbitrary(Gen.oneOf(Seq(constValue[V]))).asInstanceOf
 
 trait LowPriorityArbitrary:
 

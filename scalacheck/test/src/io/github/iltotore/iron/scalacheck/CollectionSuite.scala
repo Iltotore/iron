3cfbd9393965be8e20fa3404202cb55bb822ecf7
Iltotore <rafbodaha@gmail.com>
2023-03-03 11:04:51 +0100

    test: Add String-collection tests

--- scalacheck/test/src/io/github/iltotore/iron/scalacheck/CollectionSuite.scala
+++ scalacheck/test/src/io/github/iltotore/iron/scalacheck/CollectionSuite.scala
@@ -12,14 +12,40 @@ import utest.*
 object CollectionSuite extends TestSuite:
 
   val tests: Tests = Tests {
-    test("minLength") - testGen[Seq[Boolean], MinLength[5]]
-    test("maxLength") - testGen[Seq[Boolean], MaxLength[5]]
-    test("empty") - testGen[Seq[Boolean], Empty]
-    test("contain") - testGen[Seq[Boolean], Contain[5]]
-    test("forAll") - testGen[Seq[Boolean], ForAll[StrictEqual[true]]]
-    test("init") - testGen[Seq[Boolean], Init[StrictEqual[true]]]
-    test("tail") - testGen[Seq[Boolean], Tail[StrictEqual[true]]]
-    test("exists") - testGen[Seq[Boolean], Exists[StrictEqual[true]]]
-    test("head") - testGen[Seq[Boolean], Head[StrictEqual[true]]]
-    test("last") - testGen[Seq[Boolean], Last[StrictEqual[true]]]
+    test("minLength") {
+      test("seq") - testGen[Seq[Boolean], MinLength[5]]
+      test("string") - testGen[String, MinLength[5]]
+    }
+    test("maxLength") {
+      test("seq") - testGen[Seq[Boolean], MaxLength[5]]
+      test("string") - testGen[String, MaxLength[5]]
+    }
+    test("empty") {
+      test("seq") - testGen[Seq[Boolean], Empty]
+      test("string") - testGen[Seq[Boolean], Empty]
+    }
+    test("contain") {
+      test("seq") - testGen[Seq[Boolean], Contain[true]]
+      test("string") - testGen[String, Contain["a"]]
+    }
+    test("forAll") {
+      test("seq") - testGen[Seq[Boolean], ForAll[StrictEqual[true]]]
+      test("string") - testGen[String, ForAll[StrictEqual['a']]]
+    }
+    test("init") {
+      test("seq") - testGen[Seq[Boolean], Init[StrictEqual[true]]]
+      test("string") - testGen[String, Init[StrictEqual['a']]]
+    }
+    test("tail") {
+      test("seq") - testGen[Seq[Boolean], Tail[StrictEqual[true]]]
+      test("string") - testGen[String, Tail[StrictEqual['a']]]
+    }
+    test("head") {
+      test("seq") - testGen[Seq[Boolean], Head[StrictEqual[true]]]
+      test("string") - testGen[String, Head[StrictEqual['a']]]
+    }
+    test("last") {
+      test("seq") - testGen[Seq[Boolean], Last[StrictEqual[true]]]
+      test("string") - testGen[String, Last[StrictEqual['a']]]
+    }
   }

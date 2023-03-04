faa30f99e30b20957ec6535605b7de39c995239d
Iltotore <rafbodaha@gmail.com>
2023-03-04 10:01:44 +0100

    fix: Overflowing adjacent for min/max values

--- scalacheck/src/io/github/iltotore/iron/scalacheck/Adjacent.scala
+++ scalacheck/src/io/github/iltotore/iron/scalacheck/Adjacent.scala
@@ -24,24 +24,24 @@ object Adjacent:
 
   given Adjacent[Int] with
 
-    override def nextUp(x: Int): Int = x + 1
+    override def nextUp(x: Int): Int = if x == Int.MaxValue then x else x + 1
 
-    override def nextDown(x: Int): Int = x - 1
+    override def nextDown(x: Int): Int = if x == Int.MinValue then x else x - 1
 
   given Adjacent[Long] with
 
-    override def nextUp(x: Long): Long = x + 1
+    override def nextUp(x: Long): Long = if x == Long.MaxValue then x else x + 1
 
-    override def nextDown(x: Long): Long = x - 1
+    override def nextDown(x: Long): Long = if x == Long.MinValue then x else x - 1
 
   given Adjacent[Float] with
 
-    override def nextUp(x: Float): Float = Math.nextUp(x)
+    override def nextUp(x: Float): Float = if x == Float.MaxValue then x else Math.nextUp(x)
 
-    override def nextDown(x: Float): Float = Math.nextDown(x)
+    override def nextDown(x: Float): Float = if x == Float.MinValue then x else Math.nextDown(x)
 
   given Adjacent[Double] with
 
-    override def nextUp(x: Double): Double = Math.nextUp(x)
+    override def nextUp(x: Double): Double = if x == Double.MaxValue then x else Math.nextUp(x)
 
     override def nextDown(x: Double): Double = Math.nextDown(x)

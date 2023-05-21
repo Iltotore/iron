82fa2fc7e88f41924f4255ad6a7bb318afa1fc59
antonkw <anton@narrative.io>
2023-05-21 15:08:47 +0200

    IRON-117 zio validation

--- zio/src/io/github/iltotore/iron/zio.scala
+++ zio/src/io/github/iltotore/iron/zio.scala
@@ -27,3 +27,13 @@ object zio:
      */
     inline def refineFurtherValidation[C2](using inline constraint: Constraint[A, C2]): Validation[String, A :| (C1 & C2)] =
       (value: A).refineValidation[C2].map(_.assumeFurther[C1])
+
+  extension [A, C, T](ops: RefinedTypeOpsImpl[A, C, T])
+    /**
+     * Refine the given value applicatively at runtime, resulting in a [[Validation]].
+     *
+     * @param constraint the constraint to test with the value to refine.
+     * @return a [[Valid]] containing this value as [[T]] or an [[Validation.Failure]] containing a [[NonEmptyChunk]] of error messages.
+     */
+    inline def validation(value: A)(using inline constraint: Constraint[A, C]): Validation[String, T] =
+      value.refineValidation[C].map(_.asInstanceOf[T])

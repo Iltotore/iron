1538ffac6e6e0a0416e27de286ee76d3d0547783
Kyri Petrou <kyri.petrou@outlook.com>
2023-09-28 17:57:36 +1000

    Add scaladoc for RuntimeConstraint

--- main/src/io/github/iltotore/iron/RuntimeConstraint.scala
+++ main/src/io/github/iltotore/iron/RuntimeConstraint.scala
@@ -2,6 +2,20 @@ package io.github.iltotore.iron
 
 import scala.util.NotGiven
 
+/**
+ * A [[RuntimeConstraint]] is similar to a [[Constraint]] with the difference that it can be used
+ * in non-inlined methods.
+ *
+ * This allows refinement of values in polymorphic methods / givens without the use of `inline`.
+ * e.g., the code below would fail to compile if [[Constraint]] was used instead.
+ *
+ * {{{
+ * def foo[A, C](value: A)(using c: RuntimeConstraint[A, C]): Either[String, A :| C] =
+ *   if c.test(value) then Right(value.assume[C]) else Left(c.message)
+ * }}}
+ *
+ * In cases that one does not exist in scope, one will be automatically derived from a [[Constraint]].
+ */
 final class RuntimeConstraint[A, C](_test: A => Boolean, val message: String):
   inline def test(value: A): Boolean = _test(value)
 

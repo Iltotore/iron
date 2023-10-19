2dbc5394bb2e65ff1378ced90306826d5ce7baa8
vbergeron <valentin.bergeron@ledger.fr>
2023-10-19 11:00:38 +0200

    Support for circe KeyDecoder and KeyEncoder

--- circe/src/io/github/iltotore/iron/circe.scala
+++ circe/src/io/github/iltotore/iron/circe.scala
@@ -1,6 +1,6 @@
 package io.github.iltotore.iron
 
-import io.circe.{Decoder, DecodingFailure, Encoder, HCursor}
+import io.circe.*
 
 /**
  * Implicit [[Encoder]]s and [[Decoder]]s for refined types.
@@ -28,3 +28,27 @@ object circe:
 
   inline given[T](using mirror: RefinedTypeOps.Mirror[T], ev: Encoder[mirror.IronType]): Encoder[T] =
     ev.asInstanceOf[Encoder[T]]
+  
+  /**
+   * A [[KeyDecoder]] for refined types. Decodes to the underlying type then checks the constraint.
+   *
+   * @param decoder the [[KeyDecoder]] of the underlying type.
+   * @param constraint the [[Constraint]] implementation to test the decoded value.
+   */
+  inline given [A, B](using inline decoder: KeyDecoder[A], inline constraint: Constraint[A, B]): KeyDecoder[A :| B] =
+    KeyDecoder.instance[A :| B]: input =>
+      decoder.apply(input).flatMap(_.refineOption)
+
+  /**
+   * An [[KeyEncoder]] instance for refined types. Basically the underlying type [[KeyEncoder]].
+   *
+   * @param encoder the [[KeyEncoder]] of the underlying type.
+   */
+  inline given [A, B](using inline encoder: KeyEncoder[A]): KeyEncoder[A :| B] = encoder.asInstanceOf[KeyEncoder[A :| B]]
+
+  inline given[T](using mirror: RefinedTypeOps.Mirror[T], ev: KeyDecoder[mirror.IronType]): KeyDecoder[T] =
+    ev.asInstanceOf[KeyDecoder[T]]
+
+  inline given[T](using mirror: RefinedTypeOps.Mirror[T], ev: KeyEncoder[mirror.IronType]): KeyEncoder[T] =
+    ev.asInstanceOf[KeyEncoder[T]]
+

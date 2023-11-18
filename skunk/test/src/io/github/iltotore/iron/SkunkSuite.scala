279f312698c9a5bcd3543d9b44f83d3c92340d51
Julien Nicoulaud <julien.nicoulaud@ledger.fr>
2023-11-18 11:10:39 +0100

    Fix test suite not declared correctly

--- skunk/test/src/io/github/iltotore/iron/SkunkSuite.scala
+++ skunk/test/src/io/github/iltotore/iron/SkunkSuite.scala
@@ -13,41 +13,43 @@ object PositiveInt extends RefinedTypeOps[Int, Positive, PositiveInt]
 
 given Codec[Int] = int4
 
-val tests: Tests = Tests {
+object SkunkSuite extends TestSuite:
 
-  test("codec") {
-    test("ironType") {
-      test("success") - assert(summon[Codec[Int :| Positive]].decode(0, List(Some("5"))) == Right(5))
-      test("failure") - assert(summon[Codec[Int :| Positive]].decode(0, List(Some("-5"))).isLeft)
-      test("success") - assert(summon[Codec[Int :| Positive]].encode(5) == List(Some("5")))
+  val tests: Tests = Tests {
+
+    test("codec") {
+      test("ironType") {
+        test("success") - assert(summon[Codec[Int :| Positive]].decode(0, List(Some("5"))) == Right(5))
+        test("failure") - assert(summon[Codec[Int :| Positive]].decode(0, List(Some("-5"))).isLeft)
+        test("success") - assert(summon[Codec[Int :| Positive]].encode(5) == List(Some("5")))
+      }
+
+      test("newType") {
+        test("success") - assert(summon[Codec[PositiveInt]].decode(0, List(Some("5"))) == Right(PositiveInt(5)))
+        test("failure") - assert(summon[Codec[PositiveInt]].decode(0, List(Some("-5"))).isLeft)
+        test("success") - assert(summon[Codec[PositiveInt]].encode(PositiveInt(5)) == List(Some("5")))
+      }
     }
 
-    test("newType") {
-      test("success") - assert(summon[Codec[PositiveInt]].decode(0, List(Some("5"))) == Right(PositiveInt(5)))
-      test("failure") - assert(summon[Codec[PositiveInt]].decode(0, List(Some("-5"))).isLeft)
-      test("success") - assert(summon[Codec[PositiveInt]].encode(PositiveInt(5)) == List(Some("5")))
+    test("encoder") {
+      test("ironType") {
+        test("success") - assert(summon[Encoder[Int :| Positive]].encode(5) == List(Some("5")))
+      }
+
+      test("newType") {
+        test("success") - assert(summon[Encoder[PositiveInt]].encode(PositiveInt(5)) == List(Some("5")))
+      }
+    }
+
+    test("decoder") {
+      test("ironType") {
+        test("success") - assert(summon[Decoder[Int :| Positive]].decode(0, List(Some("5"))) == Right(PositiveInt(5)))
+        test("failure") - assert(summon[Decoder[Int :| Positive]].decode(0, List(Some("-5"))).isLeft)
+      }
+
+      test("newType") {
+        test("success") - assert(summon[Decoder[PositiveInt]].decode(0, List(Some("5"))) == Right(PositiveInt(5)))
+        test("failure") - assert(summon[Decoder[PositiveInt]].decode(0, List(Some("-5"))).isLeft)
+      }
     }
   }
-
-  test("encoder") {
-    test("ironType") {
-      test("success") - assert(summon[Encoder[Int :| Positive]].encode(5) == List(Some("5")))
-    }
-
-    test("newType") {
-      test("success") - assert(summon[Encoder[PositiveInt]].encode(PositiveInt(5)) == List(Some("5")))
-    }
-  }
-
-  test("decoder") {
-    test("ironType") {
-      test("success") - assert(summon[Decoder[Int :| Positive]].decode(0, List(Some("5"))) == Right(PositiveInt(5)))
-      test("failure") - assert(summon[Decoder[Int :| Positive]].decode(0, List(Some("-5"))).isLeft)
-    }
-
-    test("newType") {
-      test("success") - assert(summon[Decoder[PositiveInt]].decode(0, List(Some("5"))) == Right(PositiveInt(5)))
-      test("failure") - assert(summon[Decoder[PositiveInt]].decode(0, List(Some("-5"))).isLeft)
-    }
-  }
-}

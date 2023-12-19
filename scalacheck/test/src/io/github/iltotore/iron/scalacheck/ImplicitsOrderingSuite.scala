6f0598993c035b057ebd3fd4e845a087bc132691
Mateusz Wójcik <mat.wojcik@gmail.com>
2023-12-19 15:31:53 +0100

    Add test case

---
+++ scalacheck/test/src/io/github/iltotore/iron/scalacheck/ImplicitsOrderingSuite.scala
@@ -0,0 +1,11 @@
+package io.github.iltotore.iron.scalacheck
+
+import io.github.iltotore.iron.constraint.all.*
+import io.github.iltotore.iron.scalacheck.all.given
+import utest.*
+
+object ImplicitsOrderingSuite extends TestSuite:
+
+  val tests: Tests = Tests {
+    test("should resolve implicits using all.given import") - testGen[String, Empty]
+  }

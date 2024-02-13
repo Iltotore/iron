581f26b8eb4079093b209b81c1db8f90fc197b28
Raphaël Fromentin <rafbodaha@gmail.com>
2024-02-13 10:29:51 +0100

    feat: Support ZIO Prelude's `Covariant` for `MapLogic`

--- zio/test/src/io/github/iltotore/iron/ZIOSuite.scala
+++ zio/test/src/io/github/iltotore/iron/ZIOSuite.scala
@@ -4,12 +4,10 @@ import io.github.iltotore.iron.constraint.all.*
 import utest.{Show as _, *}
 
 import scala.runtime.stdLibPatches.Predef.assert
-import io.github.iltotore.iron.zio as ironZio
+import io.github.iltotore.iron.zio.{*, given}
 import _root_.zio.prelude.ZValidation
 import _root_.zio.Chunk
 import _root_.zio.NonEmptyChunk
-
-import io.github.iltotore.iron.zio.validation
 
 object ZIOSuite extends TestSuite:
   val tests: Tests = Tests {
@@ -21,5 +19,10 @@ object ZIOSuite extends TestSuite:
         Temperature.validation(0.0) ==
           ZValidation.Failure[String, String](Chunk.empty, NonEmptyChunk.single("Should be strictly positive"))
       )
+    }
+
+    test("refineAll") {
+      test - assert(Temperature.optionAll(NonEmptyChunk(1, 2, 3)).contains(NonEmptyChunk(Temperature(1), Temperature(2), Temperature(3))))
+      test - assert(Temperature.optionAll(NonEmptyChunk(1, 2, -3)).isEmpty)
     }
   }

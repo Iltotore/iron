5ce59927015c2cd0f81ce37cac4fbf593bd486d8
Raphaël Fromentin <rafbodaha@gmail.com>
2025-03-23 15:29:39 +0100

    fix: Breaking change from ZIO HTTP in ZIO form example

--- examples/formZio/src/io/github/iltotore/iron/formZio/Main.scala
+++ examples/formZio/src/io/github/iltotore/iron/formZio/Main.scala
@@ -4,4 +4,4 @@ import zio.http.*
 object Main extends ZIOAppDefault:
 
   override val run =
-    Server.serve(HttpServer.app, None).provide(Server.default)
+    Server.serve(HttpServer.routes).provide(Server.default)

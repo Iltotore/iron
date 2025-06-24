2c61ac2ba1c3932dc49ed2b97e4bef98af32af07
Darren Kim <terdong@gmail.com>
2025-06-24 22:46:57 +0900

    Add ZIO-JSON support to de-inlined given instances (#323)
    Adds the missing iron-zio-json support to the de-inlined given
    instances.
    Related to #317.

--- zioJson/src/io/github/iltotore/iron/zioJson.scala
+++ zioJson/src/io/github/iltotore/iron/zioJson.scala
@@ -3,17 +3,15 @@ package io.github.iltotore.iron
 import zio.json.{JsonDecoder, JsonEncoder}
 
 object zioJson extends ZioJsonLowPriority:
-
-
-  inline given [T](using mirror: RefinedType.Mirror[T], ev: JsonDecoder[mirror.IronType]): JsonDecoder[T] =
+  given [T](using mirror: RefinedType.Mirror[T], ev: JsonDecoder[mirror.IronType]): JsonDecoder[T] =
     ev.asInstanceOf[JsonDecoder[T]]
 
-  inline given [T](using mirror: RefinedType.Mirror[T], ev: JsonEncoder[mirror.IronType]): JsonEncoder[T] =
+  given [T](using mirror: RefinedType.Mirror[T], ev: JsonEncoder[mirror.IronType]): JsonEncoder[T] =
     ev.asInstanceOf[JsonEncoder[T]]
 
 private trait ZioJsonLowPriority:
-  inline given [A, C](using inline decoder: JsonDecoder[A], constraint: Constraint[A, C]): JsonDecoder[A :| C] =
+  given [A, C](using decoder: JsonDecoder[A], constraint: RuntimeConstraint[A, C]): JsonDecoder[A :| C] =
     decoder.mapOrFail(_.refineEither)
 
-  inline given [A, C](using inline encoder: JsonEncoder[A]): JsonEncoder[A :| C] =
+  given [A, C](using encoder: JsonEncoder[A]): JsonEncoder[A :| C] =
     encoder.asInstanceOf[JsonEncoder[A :| C]]

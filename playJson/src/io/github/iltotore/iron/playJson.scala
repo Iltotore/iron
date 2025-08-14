6ec1bf272672a8599211f5ffa2d726b4bfcbd46e
Sakis Karagiannis <sakiskar90@gmail.com>
2025-08-14 15:11:39 +0100

    Add RefinedSubtype instance to play

--- playJson/src/io/github/iltotore/iron/playJson.scala
+++ playJson/src/io/github/iltotore/iron/playJson.scala
@@ -13,6 +13,12 @@ object playJson extends PlayJsonLowPriority:
   given [T](using mirror: RefinedType.Mirror[T], ev: Writes[mirror.IronType]): Writes[T] =
     ev.asInstanceOf[Writes[T]]
 
+  given [T](using mirror: RefinedSubtype.Mirror[T], ev: Reads[mirror.IronType]): Reads[T] =
+    ev.asInstanceOf[Reads[T]]
+
+  given [T](using mirror: RefinedSubtype.Mirror[T], ev: Writes[mirror.IronType]): Writes[T] =
+    ev.asInstanceOf[Writes[T]]
+
 private trait PlayJsonLowPriority:
   /**
    * A [[Reads]] for refined types. Decodes to the underlying type then checks the constraint.

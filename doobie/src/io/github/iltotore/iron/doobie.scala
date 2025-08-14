8b2addf0452284bfbd72c87b29e1d10fe8155608
Sakis Karagiannis <sakiskar90@gmail.com>
2025-08-14 15:11:39 +0100

    Add RefinedSubtype instances to doobie

--- doobie/src/io/github/iltotore/iron/doobie.scala
+++ doobie/src/io/github/iltotore/iron/doobie.scala
@@ -8,7 +8,6 @@ import cats.Show
  */
 object doobie extends DoobieLowPriority:
 
-
   /**
    * A [[Get]] instance for new types.
    *
@@ -19,6 +18,15 @@ object doobie extends DoobieLowPriority:
   given [T](using m: RefinedType.Mirror[T], ev: Get[m.IronType]): Get[T] =
     ev.asInstanceOf[Get[T]]
 
+  /**
+   * A [[Get]] instance for new subtypes.
+   *
+   * @param m  the meta information of the refined new subtype
+   * @param ev the value getter of the underlying type
+   * @tparam T the new subtype
+   */
+  given [T](using m: RefinedSubtype.Mirror[T], ev: Get[m.IronType]): Get[T] =
+    ev.asInstanceOf[Get[T]]
 
   /**
    * A [[Put]] instance for new types.
@@ -31,6 +39,16 @@ object doobie extends DoobieLowPriority:
     ev.asInstanceOf[Put[T]]
 
   /**
+   * A [[Put]] instance for new subtypes.
+   *
+   * @param m  the meta information of the refined new subtype
+   * @param ev the value setter of the underlying type
+   * @tparam T the new subtype
+   */
+  given [T](using m: RefinedSubtype.Mirror[T], ev: Put[m.IronType]): Put[T] =
+    ev.asInstanceOf[Put[T]]
+
+  /**
    * A [[Meta]] instance for new types.
    *
    * @param m  the meta information of the refined new type
@@ -38,6 +56,16 @@ object doobie extends DoobieLowPriority:
    * @tparam T the new type
    */
   given [T](using m: RefinedType.Mirror[T], ev: Meta[m.IronType]): Meta[T] =
+    ev.asInstanceOf[Meta[T]]
+
+  /**
+   * A [[Meta]] instance for new subtypes.
+   *
+   * @param m  the meta information of the refined new subtype
+   * @param ev the value getter/setter of the underlying type
+   * @tparam T the new subtype
+   */
+  given [T](using m: RefinedSubtype.Mirror[T], ev: Meta[m.IronType]): Meta[T] =
     ev.asInstanceOf[Meta[T]]
 
 private trait DoobieLowPriority:
@@ -53,7 +81,6 @@ private trait DoobieLowPriority:
   given [A, C](using get: Get[A])(using RuntimeConstraint[A, C], Show[A]): Get[A :| C] =
     get.temap[A :| C](_.refineEither)
 
-
   /**
    * A [[Put]] instance for refined types. Retrieve values using base type's [[Put]] then check the constraint on the
    * retrieved unrefined values.
@@ -64,7 +91,6 @@ private trait DoobieLowPriority:
    */
   given [A, C](using put: Put[A])(using RuntimeConstraint[A, C], Show[A]): Put[A :| C] =
     put.tcontramap(identity)
-
 
   /**
    * A [[Meta]] instance for refined types. Retrieve values using base type's [[Meta]] then check the constraint on the
